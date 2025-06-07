1bf452a89bd97e3b1079f5ea13054562cc4c288b
DBhatkar009 <dhananjay.bhatkar999@outlook.com>
2025-06-07 13:40:06 +0530

    Typescript Modules Import Export method

---
+++ fundamentals/10-modules-export.ts
@@ -0,0 +1,13 @@
+import { courseTypeAll } from './10-modules-import';
+// this module exports constants related to a course
+export const COURSE_TYPE = 'TypeScript Fundamentals';
+export const COURSE_DURATION = 30; // in hours
+export const COURSE_AUTHOR = 'john doe'
+
+export const COURSE_LEVEL = {
+    beginner: 'Beginner',
+    intermediate: 'Intermediate',
+    advanced: 'Advanced'
+}
+
+const COURSE_TOPICS = ['Introduction', 'Types', 'Functions', 'Classes', 'Interfaces', 'Modules', 'Generics'];
