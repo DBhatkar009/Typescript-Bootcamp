4d37c115a5f2fac33c243ca1e32300c5daa10b40
DBhatkar009 <dhananjay.bhatkar999@outlook.com>
2025-06-20 23:49:17 +0530

    Working on files with main.ts

--- fundamentals/18-object-create-notation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var title = "learn Nodejs", description = "A comprehensive guide to mastering Node.js.", price = 49.99;
-var coursePurchase = {
-    title: title,
-    description: description,
-    price: price
-};
-console.log("coursePurchase:", coursePurchase);
--- fundamentals/23-intersection-types.js
+++ fundamentals/23-intersection-types.js
@@ -1,9 +1,9 @@
 "use strict";
-var ExampleBike = {
+const ExampleBike = {
     id: "254545-jjfd-45648vf-56464-vv4445",
     title: "Modern Bikes",
     model: "BMW",
     Description: "BMW-Sport Bike",
     price: 9000
 };
-console.log("this is the Bike show room: ".concat(ExampleBike));
+console.log(`this is the Bike show room: ${ExampleBike}`);
