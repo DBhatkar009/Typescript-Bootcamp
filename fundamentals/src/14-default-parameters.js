b583a3fc6153b95ce63e5d448cfa4317c06cc221
DBhatkar009 <dhananjay.bhatkar999@outlook.com>
2025-06-21 12:44:31 +0530

    tsconfig outDir & rootDir feature

--- fundamentals/src/14-default-parameters.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Typescript allows you to set default values for function parameters.
-// This is useful when you want to provide a default value for a parameter if it is not provided by the caller.
-function greet(name = "guest", age = 18, isDriversLicenseHolder = true) {
-    console.log(`Hello ${name}, you are ${age} years old and your driver's license status is: ${isDriversLicenseHolder ? "Holder" : "Not a holder"}`);
-}
-greet(); // Output: Hello guest, you are 18 years old and your driver's license status is: Not a holder
--- fundamentals/src/18-object-create-notation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const title = "learn Nodejs", description = "A comprehensive guide to mastering Node.js.", price = 49.99;
-const coursePurchase = {
-    title,
-    description,
-    price
-};
-console.log("coursePurchase:", coursePurchase);
