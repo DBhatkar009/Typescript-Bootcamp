d76dd389f55f2d557743d4ce5e4bc85169660ca4
DBhatkar009 <dhananjay.bhatkar999@outlook.com>
2025-06-26 11:17:53 +0530

    Typescript Understanding on Static Class & Static Variables Also Object Oriented Inheritance

--- classes/01-introduce-classes.js
+++ classes/01-introduce-classes.js
@@ -1,18 +1,26 @@
 // Classes 
 var Course = /** @class */ (function () {
-    //    private title: string;
-    //    private subtitle: string;
-    //    private creationDt: Date;
-    function Course(ntitle, subtitle, creationDt) {
+    function Course(ntitle, price, subtitle, creationDt) {
+        // this.ntitle = ntitle;
+        // this.subtitle = subtitle;
+        // this.creationDt = creationDt;
         if (subtitle === void 0) { subtitle = ''; }
         if (creationDt === void 0) { creationDt = new Date(2025, 1, 14); }
         this.ntitle = ntitle;
+        this.price = price;
         this.subtitle = subtitle;
         this.creationDt = creationDt;
-        this.ntitle = ntitle;
-        this.subtitle = subtitle;
-        this.creationDt = creationDt;
+        this.validate(this);
+        Course.Total_Course++;
     }
+    Course.prototype.validate = function (course) {
+        if (course.price <= 0) {
+            throw new Error("Price must be greater then Zero!!");
+        }
+    };
+    Course.printTitle = function (course) {
+        console.log("Our New Course Title printed: ".concat(course.title));
+    };
     Object.defineProperty(Course.prototype, "title", {
         get: function () {
             return this.ntitle;
@@ -34,10 +42,21 @@ var Course = /** @class */ (function () {
         enumerable: false,
         configurable: true
     });
+    //    private title: string;
+    //    private subtitle: string;
+    //    private creationDt: Date;
+    Course.Total_Course = 0;
+    Course.NEW_TITLE = "Typescript Course";
     return Course;
 }());
-var typescript = new Course("Typescript Bootcamp");
+Course.Total_Course;
+// Course.NEW_TITLE = '';
+var typescript = new Course(Course.NEW_TITLE, 0);
 // typescript.title = "New Title Appear here!";
 console.log(typescript.title);
-var Angular = new Course("Angular Bootcamp");
+Course.printTitle(typescript);
+var Angular = new Course("Angular Bootcamp", 0);
 console.log(Angular.title);
+console.log(Course.NEW_TITLE);
+console.log(Course.Total_Course);
+console.log();
--- classes/01-introduce-classes.ts
+++ classes/01-introduce-classes.ts
@@ -5,14 +5,34 @@ class Course {
 //    private subtitle: string;
 //    private creationDt: Date;
      
+     static Total_Course = 0;
+
+    static NEW_TITLE = "Typescript Course";
+
     constructor(
     private ntitle: string,
+    private price: number,
     private subtitle = '',
     private creationDt = new Date(2025,1,14)
 ) {
-        this.ntitle = ntitle;
-        this.subtitle = subtitle;
-        this.creationDt = creationDt;
+        // this.ntitle = ntitle;
+        // this.subtitle = subtitle;
+        // this.creationDt = creationDt;
+
+        this.validate(this);
+        Course.Total_Course++;
+
+    }
+
+       validate(course: Course){
+         if(course.price <=0){
+            throw new Error("Price must be greater then Zero!!");
+            
+         }
+    }
+     
+    static printTitle(course: Course){ 
+        console.log(`Our New Course Title printed: ${course.title}`);
     }
 
     get title() {
@@ -34,10 +54,17 @@ class Course {
     }
 }
 
-const typescript = new Course("Typescript Bootcamp");
+Course.Total_Course;
+// Course.NEW_TITLE = '';
+const typescript = new Course(Course.NEW_TITLE, 0);
 
 // typescript.title = "New Title Appear here!";
 console.log(typescript.title);
+Course.printTitle(typescript);
 
-const Angular = new Course("Angular Bootcamp");
+const Angular = new Course("Angular Bootcamp", 0);
 console.log(Angular.title);
+console.log(Course.NEW_TITLE);
+console.log(Course.Total_Course);
+console.log();
+
